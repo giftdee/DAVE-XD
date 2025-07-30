ba108728aaea5cac334f28e2467f282be6f60317
giftdee <discordbotz526@gmail.com>
2025-07-30 19:20:21 +0300

    Update level.js

--- bdd/level.js
+++ bdd/level.js
@@ -7,7 +7,7 @@ const { Pool } = require("pg");
 const s = require("../set");
 
 // Retrieve the database URL from the s.DATABASE_URL variable
-const dbUrl = s.DATABASE_URL ? s.DATABASE_URL : "postgres://db_7xp9_user:6hwmTN7rGPNsjlBEHyX49CXwrG7cDeYi@dpg-cj7ldu5jeehc73b2p7g0-a.oregon-postgres.render.com/db_7xp9";
+const dbUrl = process.env.DATABASE_URL || s.DATABASE_URL || "postgres://db_7xp9_user:6hwmTN7rGPNsjlBEHyX49CXwrG7cDeYi@dpg-cj7ldu5jeehc73b2p7g0-a.oregon-postgres.render.com/db_7xp9";
 const proConfig = {
   connectionString: dbUrl,
   ssl: {
