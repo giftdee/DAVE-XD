ba69b5a49ffb64089aacf2735cebdf3210195254
giftdee <discordbotz526@gmail.com>
2025-08-03 01:41:16 +0300

    Update config.js

--- davedb/config.js
+++ davedb/config.js
@@ -1,5 +1,5 @@
 const { Pool } = require('pg');
-const { database } = require('../Env/settings');
+const { database } = require('../set');
 
 const pool = new Pool({
     connectionString: database,
