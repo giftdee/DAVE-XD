d817eee0531e2fdb91fb4d3c962f0fa206de30f8
giftdee <discordbotz526@gmail.com>
2025-08-05 15:05:13 +0300

    Update events.js

--- daveplugins/events.js
+++ daveplugins/events.js
@@ -1,46 +1,34 @@
 const { zokou } = require('../framework/zokou');
 const { attribuerUnevaleur } = require('../bdd/welcome');
 
+
 async function events(nomCom) {
     zokou({
         nomCom: nomCom,
-        categorie: 'Group',
-        reaction: '⚙️'
+        categorie: 'Group'
     }, async (dest, zk, commandeOptions) => {
-        const { ms, arg, repondre, superUser, verifAdmin, nomAuteurMessage } = commandeOptions;
+        const { ms, arg, repondre, superUser, verifAdmin } = commandeOptions;
 
-        if (!verifAdmin && !superUser) {
-            return repondre(`𝐃𝐀𝐕𝐄-𝐗𝐌𝐃\n\n◈━━━━━━━━━━━━━━━━◈\n│❒ Yo ${nomAuteurMessage}, you ain’t got the keys to mess with ${nomCom}! 😡 Only admins or Gifted_dave can run 𝐃𝐀𝐕𝐄-𝐗𝐌𝐃’s group vibes! 🚫\n◈━━━━━━━━━━━━━━━━◈`);
-        }
+        if (verifAdmin || superUser) {
+            if (!arg[0] || arg.join(' ') === ' ') {
+                repondre(nomCom + ' ' + ' on to active and ' + ' ' + nomCom + ' ' + 'off to put off');
+            } else {
+                if (arg[0] === 'on' || arg[0] === 'off') {
 
-        if (!arg[0] || arg.join(' ').trim() === '') {
-            return repondre(`𝐃𝐀𝐕𝐄-𝐗𝐌𝐃\n\n◈━━━━━━━━━━━━━━━━◈\n│❒ Yo ${nomAuteurMessage}, don’t be lazy! Use *${nomCom} on* to activate or *${nomCom} off* to shut it down! 😎 𝐃𝐀𝐕𝐄-𝐗𝐌𝐃 needs clear orders! 🔥\n◈━━━━━━━━━━━━━━━━◈`);
-        }
-
-        const setting = arg[0].toLowerCase();
-        if (setting === 'on' || setting === 'off') {
-            try {
-                await attribuerUnevaleur(dest, nomCom, setting);
-                await zk.sendMessage(
-                    dest,
-                    {
-                        text: `𝐃𝐀𝐕𝐄-𝐗𝐌𝐃\n\n◈━━━━━━━━━━━━━━━━◈\n│❒ BOOM, ${nomAuteurMessage}! ${nomCom} is now ${setting} for this group! 🔥\n│❒ 𝐃𝐀𝐕𝐄-𝐗𝐌𝐃’s got it locked in! 🚀\n│❒ Powered by Gifted_dave\n◈━━━━━━━━━━━━━━━━◈`,
-                        footer: `Hey ${nomAuteurMessage}! I'm DAVE-XMD, created by gifted_dave 😎`
-                    },
-                    { quoted: ms }
-                );
-            } catch (error) {
-                console.error(`Error updating ${nomCom}:`, error);
-                await repondre(`𝐃𝐀𝐕𝐄-𝐗𝐌𝐃\n\n◈━━━━━━━━━━━━━━━━◈\n│❒ TOTAL BUST, ${nomAuteurMessage}! 𝐃𝐀𝐕𝐄-𝐗𝐌𝐃 tripped while setting ${nomCom}: ${error.message} 😡 Try again or flop! 😣\n◈━━━━━━━━━━━━━━━━◈`);
+                    await attribuerUnevaleur(dest, nomCom, arg[0]);
+                    repondre( nomCom + "is actualised on " + arg[0]);
+                } else {
+                    repondre('on for active and off for desactive');
+                }
             }
         } else {
-            repondre(`𝐃𝐀𝐕𝐄-𝐗𝐌𝐃\n\n◈━━━━━━━━━━━━━━━━◈\n│❒ Yo ${nomAuteurMessage}, what’s this nonsense? 😡 Only *${nomCom} on* or *${nomCom} off* works for 𝐃𝐀𝐕𝐄-𝐗𝐌𝐃 ! Get it right! 🔧\n◈━━━━━━━━━━━━━━━━◈`);
+            repondre('You can\'t use this commands ');
         }
     });
 }
 
-// Register the commands
+// Appel de la fonction events pour les valeurs 'welcome' et 'goodbye'
 events('welcome');
 events('goodbye');
 events('antipromote');
-events('antidemote');
+events('antidemote') ;
