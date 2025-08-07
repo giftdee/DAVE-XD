4ba593dbf4ba340956930d348488d0962d76ce29
giftdee <discordbotz526@gmail.com>
2025-08-07 15:16:20 +0300

    Update stickersearch.js

--- daveplugins/stickersearch.js
+++ daveplugins/stickersearch.js
@@ -4,7 +4,7 @@ const {zokou} = require("../framework/zokou");
 
 zokou({
   nomCom: "stickersearch",
-  categorie: 'Search',
+  categorie: 'Dave-Search',
   reaction: "🍁"
 },
 async (dest, zk, commandeOptions) => {
