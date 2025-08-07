e16ea9eb8263e32d555623480e9b36c9f453e8a2
giftdee <discordbotz526@gmail.com>
2025-08-07 10:55:15 +0300

    giftdee

--- daveplugins/daveai.js
+++ daveplugins/daveai.js
@@ -6,7 +6,7 @@ const { default: axios } = require('axios');
 
 
 
-zokou({nomCom:"davlo",reaction:"✨",categorie:"AI"},async(dest,zk,commandeOptions)=>{
+zokou({nomCom:"davlo",reaction:"✨",categorie:"Dave-Ai"},async(dest,zk,commandeOptions)=>{
 
   const {repondre,ms,arg}=commandeOptions;
 
@@ -45,7 +45,7 @@ fetch(`http://api.brainshop.ai/get?bid=177607&key=NwzhALqeO1kubFVD&uid=[uid]&msg
 
 
 
-  zokou({ nomCom: "bing4", reaction: "✨", categorie: "AI" }, async (dest, zk, commandeOptions) => {
+  zokou({ nomCom: "bing4", reaction: "✨", categorie: "Dave-Ai" }, async (dest, zk, commandeOptions) => {
     const { repondre, arg, ms } = commandeOptions;
 
     try {
@@ -73,7 +73,7 @@ fetch(`http://api.brainshop.ai/get?bid=177607&key=NwzhALqeO1kubFVD&uid=[uid]&msg
     }
   });
 
-  zokou({ nomCom: "ai2", reaction: "✨", categorie: "AI" }, async (dest, zk, commandeOptions) => {
+  zokou({ nomCom: "ai2", reaction: "✨", categorie: "Dave-Ai" }, async (dest, zk, commandeOptions) => {
     const { repondre, arg, ms } = commandeOptions;
 
     try {
@@ -98,7 +98,7 @@ fetch(`http://api.brainshop.ai/get?bid=177607&key=NwzhALqeO1kubFVD&uid=[uid]&msg
   });
 
 
-zokou({ nomCom: "gpt-all", reaction: "✨", categorie: "AI" }, async (dest, zk, commandeOptions) => {
+zokou({ nomCom: "gpt-all", reaction: "✨", categorie: "Dave-Ai" }, async (dest, zk, commandeOptions) => {
     const { repondre, arg, ms } = commandeOptions;
 
     try {
