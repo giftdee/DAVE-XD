f7beb76933c869b21567a63f39db7a0c8eb394d2
giftdee <discordbotz526@gmail.com>
2025-08-07 12:20:31 +0300

    giftdee

--- daveplugins/igdl-fb-tk.js
+++ daveplugins/igdl-fb-tk.js
@@ -2,72 +2,170 @@ const {zokou} = require('../framework/zokou');
 const fs = require('fs');
 const getFBInfo = require("@xaviabot/fb-downloader");
 const { default: axios } = require('axios');
+const { getJson } = require("../framework/utils");
 
-zokou({nomCom : "instagram" , categorie : "Download"},async (dest , zk , commandeOptions)=>{
+zokou({nomCom : "igdl" , categorie : "Dave-Download"},async (dest , zk , commandeOptions)=>{
   const {ms,repondre,arg} = commandeOptions ;
 
   let link = arg.join(' ')
 
-  if (!arg[0]) { repondre('𝐏𝐥𝐞𝐚𝐬𝐞 𝐢𝐧𝐬𝐞𝐫𝐭 𝐚𝐧 𝐈𝐧𝐬𝐭𝐚𝐠𝐫𝐚𝐦 𝐯𝐢𝐝𝐞𝐨 𝐥𝐢𝐧𝐤');return}; 
+  if (!arg[0]) { repondre('Provide Instagram video link to download');return}; 
 
   try {
-    let igvid = await axios('https://api.dreaded.site/api/igdl?url='+link)
+
+    let igvid = await axios('https://api.vihangayt.com/downloader/ig?url='+link)
 
     if (igvid.data.data.data[0].type == 'video') {
-      zk.sendMessage(dest,{video : {url : igvid.data.data.data[0].url},caption : "𝐈𝐆 𝐕𝐢𝐝𝐞𝐨 𝐃𝐨𝐰𝐧𝐥𝐨𝐚𝐝𝐞𝐫 (𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐛𝐲 𝐃𝐀𝐕𝐄-𝐗𝐌𝐃)",gifPlayback : false },{quoted : ms}) 
+    zk.sendMessage(dest,{video : {url : igvid.data.data.data[0].url},caption : "ig video downloader powered by *☢️LUCKY MD X-FORCE☢️*",gifPlayback : false },{quoted : ms}) 
     }
     else {
-      zk.sendMessage(dest,{image : {url : igvid.data.data.data[0].url},caption : "𝐈𝐆 𝐈𝐦𝐚𝐠𝐞 𝐃𝐨𝐰𝐧𝐥𝐨𝐚𝐝𝐞𝐫 (𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐛𝐲 𝐃𝐀𝐕𝐄-𝐗𝐌𝐃)"})
+        zk.sendMessage(dest,{image : {url : igvid.data.data.data[0].url},caption : "ig image downloader powered by *☢️LUCKY MD X-FORCE☢️*"})
     }
-  } catch (e) { repondre("𝐄𝐫𝐫𝐨𝐫 𝐨𝐜𝐜𝐮𝐫𝐫𝐞𝐝 𝐝𝐮𝐫𝐢𝐧𝐠 𝐝𝐨𝐰𝐧𝐥𝐨𝐚𝐝:\n" + e) }
+
+  } catch (e) {repondre("erreur survenue lors du téléchargement \n " + e)}
+
 });
 
-zokou({ nomCom: "facebook", categorie: "Download", reaction: "📽️" }, async (dest, zk, commandeOptions) => {
+
+zokou({
+  nomCom: "fbdl",
+  categorie: "Dave-Download",
+  reaction: "📽️"
+},
+async (dest, zk, commandeOptions) => {
   const { repondre, ms, arg } = commandeOptions;
 
   if (!arg[0]) {
-    repondre('𝐈𝐧𝐬𝐞𝐫𝐭 𝐚 𝐩𝐮𝐛𝐥𝐢𝐜 𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐯𝐢𝐝𝐞𝐨 𝐥𝐢𝐧𝐤!');
+    repondre('Insert a public facebook video link!');
     return;
   }
 
   const queryURL = arg.join(" ");
 
   try {
-    getFBInfo(queryURL).then((result) => {
-      let caption = `𝐓𝐢𝐭𝐥𝐞: ${result.title}\n𝐋𝐢𝐧𝐤: ${result.url}`;
-      zk.sendMessage(dest, { image: { url: result.thumbnail }, caption: caption }, { quoted: ms });
-      zk.sendMessage(dest, { video: { url: result.hd }, caption: '𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐯𝐢𝐝𝐞𝐨 𝐝𝐨𝐰𝐧𝐥𝐨𝐚𝐝𝐞𝐫 (𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐛𝐲 𝐃𝐀𝐕𝐄-𝐗𝐌𝐃)' }, { quoted: ms });
-    }).catch((error) => {
-      console.log("Error:", error);
-      repondre('𝐓𝐫𝐲 𝐟𝐛𝐝𝐥𝟐 𝐨𝐧 𝐭𝐡𝐢𝐬 𝐥𝐢𝐧𝐤');
-    });
+     getFBInfo(queryURL)
+    .then((result) => {
+       let caption = `
+        titre: ${result.title}
+        Lien: ${result.url}
+      `;
+       zk.sendMessage(dest,{image : { url : result.thumbnail}, caption : caption},{quoted : ms}) ;
+       zk.sendMessage(dest, { video: { url: result.hd  }, caption: 'facebook video downloader powered by *☢️LUCKY MD X-FORCE☢️*' }, { quoted: ms });
+
+    })
+    .catch((error) => {console.log("Error:", error)
+                      repondre('try fbdl2 on this link')});
+
+
+
   } catch (error) {
-    console.error('Error:', error);
-    repondre('𝐃𝐨𝐰𝐧𝐥𝐨𝐚𝐝 𝐞𝐫𝐫𝐨𝐫: ' + error);
+    console.error('Erreur lors du téléchargement de la vidéo :', error);
+    repondre('Erreur lors du téléchargement de la vidéo.' , error);
   }
 });
 
-zokou({ nomCom: "facebook2", categorie: "Download", reaction: "📽️" }, async (dest, zk, commandeOptions) => {
+
+
+zokou({ nomCom: "tiktok", categorie: "Dave-Download", reaction: "🎵" }, async (dest, zk, commandeOptions) => {
+  const { arg, ms, prefixe,repondre } = commandeOptions;
+  if (!arg[0]) {
+    repondre(`how to use this command:\n ${prefixe}tiktok tiktok_video_link`);
+    return;
+  }
+
+  const videoUrl = arg.join(" ");
+
+ let data = await axios.get('https://api.onesytex.my.id/api/tiktok-dl='+ videoUrl) ;
+
+  let tik = data.data.data
+
+      // Envoi du message avec le thumbnail de la vidéo
+      const caption = `
+Author: ${tik.author}
+Description: ${tik.desc}
+      `;
+
+
+      zk.sendMessage(dest, { video: { url: tik.links[0].a} , caption : caption },{quoted : ms});    
+
+
+});
+
+zokou({
+  nomCom: "fbdl2",
+  categorie: "Dave-Download",
+  reaction: "📽️"
+},
+async (dest, zk, commandeOptions) => {
   const { repondre, ms, arg } = commandeOptions;
 
   if (!arg[0]) {
-    repondre('𝐈𝐧𝐬𝐞𝐫𝐭 𝐚 𝐩𝐮𝐛𝐥𝐢𝐜 𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐯𝐢𝐝𝐞𝐨 𝐥𝐢𝐧𝐤!');
+    repondre('Insert a public facebook video link! !');
     return;
   }
 
   const queryURL = arg.join(" ");
 
   try {
-    getFBInfo(queryURL).then((result) => {
-      let caption = `𝐓𝐢𝐭𝐥𝐞: ${result.title}\n𝐋𝐢𝐧𝐤: ${result.url}`;
-      zk.sendMessage(dest, { image: { url: result.thumbnail }, caption: caption }, { quoted: ms });
-      zk.sendMessage(dest, { video: { url: result.sd }, caption: '𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐯𝐢𝐝𝐞𝐨 𝐝𝐨𝐰𝐧𝐥𝐨𝐚𝐝𝐞𝐫 (𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐛𝐲 𝐃𝐀𝐕𝐄-𝐗𝐌𝐃)' }, { quoted: ms });
-    }).catch((error) => {
-      console.log("Error:", error);
-      repondre(error);
-    });
+     getFBInfo(queryURL)
+    .then((result) => {
+       let caption = `
+        titre: ${result.title}
+        Lien: ${result.url}
+      `;
+       zk.sendMessage(dest,{image : { url : result.thumbnail}, caption : caption},{quoted : ms}) ;
+       zk.sendMessage(dest, { video: { url: result.sd  }, caption: 'facebook video downloader powered by *☢️LUCKY MD X-FORCE☢️*' }, { quoted: ms });
+
+    })
+    .catch((error) => {console.log("Error:", error)
+                      repondre(error)});
+
+
+
   } catch (error) {
-    console.error('Error:', error);
-    repondre('𝐃𝐨𝐰𝐧𝐥𝐨𝐚𝐝 𝐞𝐫𝐫𝐨𝐫: ' + error);
+    console.error('Erreur lors du téléchargement de la vidéo :', error);
+    repondre('Erreur lors du téléchargement de la vidéo.' , error);
+  }
+});
+
+// Request for Muvies
+zokou({
+  nomCom: "movie",
+  categorie: "Dave-Search",
+  reaction: "🎥"
+}, async (origineMessage, zk, commandeOptions) => {
+  const { repondre, arg } = commandeOptions;
+
+  if (!arg[0]) {
+    repondre("Quel film veux-tu ?");
+    return;
+  }
+
+  const movieTitle = arg.join(" ");
+  const movie = await getJson(
+    `http://www.omdbapi.com/?apikey=742b2d09&t=${encodeURIComponent(movieTitle)}&plot=full`
+  );
+
+  if (movie.Response !== 'True') {
+    repondre('*Not found*');
+    return;
+  }
+
+  let msg = '';
+  const url = movie.Poster;
+  delete movie.Poster;
+  delete movie.Response;
+  delete movie.Ratings;
+
+  for (const data in movie) {
+    if (movie[data] !== 'N/A') {
+      msg += `*${data} :* ${movie[data]}\n`;
+    }
+  }
+
+  if (url === 'N/A') {
+    repondre(msg.trim());
+  } else {
+    await zk.sendMessage(origineMessage.from, { url }, { caption: msg.trim() });
   }
 });
