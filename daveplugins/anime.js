8c31467e7d23aeb3c045f15536a863d129ea4a95
giftdee <discordbotz526@gmail.com>
2025-08-08 12:38:41 +0300

    Update anime.js

--- daveplugins/anime.js
+++ daveplugins/anime.js
@@ -160,7 +160,7 @@ zokou({
 
 zokou({
   nomCom: "emojimix",
-  categorie: "Dave-conversion"
+  categorie: "Dave-Conversion"
 }, async (dest, zk, commandeOptions) => {
   const { arg, repondre, ms, nomAuteurMessage } = commandeOptions;
 
