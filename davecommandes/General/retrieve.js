2c3ea673c36b6622a89767dda0e31033871e18da
Gifted-Dave <discordbotz526@gmail.com>
2025-09-24 17:10:50 +0300

    🔥 history

---
+++ davecommandes/General/retrieve.js
@@ -0,0 +1,26 @@
+module.exports = async (context) => {
+    const { client, m } = context;
+
+    if (!m.quoted) return;
+
+    const quotedMessage = m.msg?.contextInfo?.quotedMessage;
+
+    try {
+        if (quotedMessage?.imageMessage) {
+            const imageUrl = await client.downloadAndSaveMediaMessage(quotedMessage.imageMessage);
+            await client.sendMessage(client.user.id, { 
+                image: { url: imageUrl },
+                caption: "Rҽƚɾιҽʋҽԃ  Ⴆყ 𝙳𝙰𝚅𝙴-x𝙳"
+            });
+        } 
+        else if (quotedMessage?.videoMessage) {
+            const videoUrl = await client.downloadAndSaveMediaMessage(quotedMessage.videoMessage);
+            await client.sendMessage(client.user.id, {
+                video: { url: videoUrl },
+                caption: "Rҽƚɾιҽʋҽԃ  Ⴆყ 𝙳𝙰𝚅𝙴-x𝙳"
+            });
+        }
+    } catch (error) {
+       
+    }
+};
